0f2f78c215395b77ea54c62cbd41847cf9ce0f11
joltzz <guijsilva02@gmail.com>
2022-02-01 12:41:54 -0300

    cartas aparecendo ao digitar a quantidade correta

--- script.js
+++ script.js
@@ -5,14 +5,21 @@ while(qtdCartas<4 || qtdCartas>14 || qtdCartas % 2!=0){
     qtdCartas=parseInt(prompt("Digite a quantidade de cartas que deseja entre 4 e 14: "))
 }
 
-//criando um for para criar as cartas no HTML
+//criando um for para criar as cartas solicitadas
 let criarCarta="";
-for(let i=0; i<=qtdCartas; i++){
+for(let i=0; i<qtdCartas; i++){
     //`percore ${qtdCartas} vezes criando ${qtdCartas} cartas`
-    criarCarta+=(`div id="carta${i}" class="carta" onclick="girarCarta(this)"></div>`);
+    criarCarta+=(`<div id="carta${i}" class="carta" onclick="mostrarCarta(this)"></div>`);
 }
 
-//funcao de girar a carta
-function girarCarta(){
+//aplica a carta no HTML
+let adicionaCarta=document.querySelector(".cartas")
+adicionaCarta.innerHTML=criarCarta;
 
+//aplica imagem no verso da carta
+let cartas =adicionaCarta.children;
+for(let carta of cartas){
+    carta.innerHTML = "<img src='img/front.png'>";
 }
+
+//resta centralizar/armazenar/sortear/comparar//
