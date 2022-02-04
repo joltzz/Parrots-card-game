6da59a626057fd832db3eacdfb31b5e6cb22874f
joltzz <guijsilva02@gmail.com>
2022-02-04 16:12:00 -0300

    começo da comparação das imagens

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let qtdCartas=0;
 let arrayCartas=["gif0","gif1","gif2","gif3","gif4","gif5","gif6"];
 let primeiraCarta=null;
 let segundaCarta=null;
+let trava=false;
 let contador=0;
 let tempo=0;//cronometrar o tempo//
 let relogio;
@@ -20,7 +21,7 @@ function iniciarJogo(){
     //criando um for para criar as cartas solicitadas no arquivo html
     let criarCarta='';
     for(let i=0;i<sorteador.length;i++){
-        criarCarta+=`<div data-identifier="card" class="carta" onclick="mostrarCarta(this)"><div data-identifier=ront-face" class="cartaFrente"><img src='./gif/${sorteador[i]}.gif'></div><div data-identifier="back-face" class="cartaTras"><img src="./img/front.png"></div></div>`
+        criarCarta+=`<div data-identifier="card" class="carta" onclick="mostrarCarta(this)"><div data-identifier=front-face" class="cartaFrente"><img src='gif/${sorteador[i]}.gif'></div><div data-identifier="back-face" class="cartaTras"><img src="./img/front.png"></div></div>`
     }
     //adiciona as cartas embaralhadas no html
     const adicionaCarta=document.querySelector("main");
@@ -33,16 +34,51 @@ function iniciarJogo(){
     },1000);
 }
 
-//aplica imagem no verso da carta
-let cartas =adicionaCarta.children;
-for(let carta of cartas){
-    carta.innerHTML = '<div class="verso-face" data-identifier="back-face"><img src="img/front.png"></div>'+
-    '<div class="frente-face" data-identifier="front-face"> *********** </div>';
+function mostrarCarta(carta){
+    //verifica se a carta ja esta virada
+    if (!trava && !carta.classList.contains("flip")){
+        carta.classList.add("flip")
+        //coleta da primeira carta
+        if(!primeiraCarta){
+            primeiraCarta=carta.querySelector(".cartaFrente img");//seleciona a primeira carta
+        }
+        else if(!segundaCarta){
+            segundaCarta=carta.querySelector(".cartaFrente img")//seleciona a segunnda carta
+        }//VERIFICAÇÃO DAS DUAS IMAGENS SELECIONADAS
+        if(primeiraCarta.src==segundaCarta.src){
+            //adiciona a classe das cartas iguais
+            primeiraCarta.parentNode.parentNode.classList.add("cartasIguais");
+            segundaCarta.parentNode.parentNode.classList.add("cartasIguais");
+            //reseta as variaveis de comparação
+            primeiraCarta=null;
+            segundaCarta=null;
+            contador=contador+2;
+        }
+
+
+
+
+
+
+
+
+
+
+    }
 }
 
-//funcao quando o usuario clicar na carta
-function clickCarta(){
-    
-    //when clicked, its supposed to replace the front image(parrot)
-    //take img/front out ---> replace with Math.random gif
+//função para verificar se o jogo ja terminou
+function verificaFim(){
+    let fim=true;
+    //seleciona todas as cartas do <main>
+    let cartas=document.querySelectorAll(".carta");
+    for(let carta of cartas){
+        //verificação se todas as cartas do <main> possuem .flip
+        if(!carta.classList.contains("flip")){
+            fim=false;
+        }
+    }
+    return fim;
 }
+
+iniciarJogo();
